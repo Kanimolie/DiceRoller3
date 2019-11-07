3d0a9936605aecbb98b4f3c8f84a875790e9debf
mithrah <skanimolie@gmail.com>
2019-11-07 18:07:42 +0000

    Initial commit

---
+++ app/src/main/java/com/example/diceroller3/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.diceroller3;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> arrayQuestions = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        arrayQuestions.add("If you could go anywhere in the world, where would you go?");
+        arrayQuestions.add("If you were stranded on a desert island, what three things would you want to take with you?");
+        arrayQuestions.add("If you could eat only one food for the rest of your life, what would that be?");
+        arrayQuestions.add("If you won a million dollars, what is the first thing you would buy?\n");
+        arrayQuestions.add("If you could spaned the day with one fictional character, who would it be?\n");
+        arrayQuestions.add("If you found a magic lantern and a genie gave you three wishes, what would you wish?\n");
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clickButton(View view) {
+
+        Random r = new Random();
+        int generatednum = r.nextInt(6);
+
+        String question = arrayQuestions.get(generatednum);
+        TextView tv = findViewById(R.id.questionTextView);
+        tv.setText(question);
+    }
+}
+
